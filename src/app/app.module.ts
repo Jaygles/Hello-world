4a78a6f6a83aa4030cbaf5f25bf3a09b0386cc61
Jay Sullivan <JaySull514@gmail.com>
2018-02-10 12:21:06 -0600

    Adding get request test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
@@ -12,7 +12,8 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
     HelloWorldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
 
 let emitter;
 const observable = Observable.create(e => emitter = e);
@@ -18,16 +21,32 @@ observable.subscribe(observer);
 emitter.next('Hello world');
 emitter.complete();
 
+const weather = Observable.of();
+weather.subscribe(observer);
+
 @Component({
   selector: 'app-hello-world',
   templateUrl: './hello-world.component.html',
   styleUrls: ['./hello-world.component.css']
 })
+
 export class HelloWorldComponent implements OnInit {
+  title;
+  url = 'https://api.openweathermap.org/data/2.5/weather?q=Kansas&APPID=6cc7c6ab8b2be4130c22373b991a01d4';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
   }
 
+  getWeather() {
+    Observable.of(this.http.get(this.url).subscribe(res => {
+      console.log(res);
+      this.title = JSON.stringify(res);
+    }));
+  }
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
